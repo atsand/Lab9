0d09e0449da4955c0bb3383eb1199509bd96c181
Aaron <atsand23@gmail.com>
2018-02-01 15:46:50 -0500

    First Commit
    First Commit

---
+++ Lab9/Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    public class Circle
+    {
+        private double radius;
+        private const double PI = Math.PI;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double CalculateCircumference()
+        {
+            double circumference = this.radius * 2 * PI;
+            return circumference;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return FormatedNumber(CalculateCircumference());
+        }
+
+        public double CalculateArea()
+        {
+            double area = PI * Math.Pow(this.radius, 2);
+            return area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            return FormatedNumber(CalculateArea());
+        }
+
+        private string FormatedNumber(double x)
+        {
+            return Math.Round(x,2).ToString();
+        }
+
+        public void SetRadius(double r)
+        {
+            this.radius = r;
+        }
+
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the circle builder.");
+            Start();
+        }
+
+        private static double CheckInput()
+        {
+            double input;
+            Console.WriteLine();
+            Console.WriteLine("Please enter a radius:");
+            if (double.TryParse(Console.ReadLine(), out input))
+            {
+                return input;
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Input is not valid");
+                return CheckInput();
+            }
+        }
+
+        private static void Start()
+        {
+            bool again = true;
+            int count = 0;
+            while (again)
+            {
+                double radius;
+                Circle newCircle;
+                radius = CheckInput();
+                newCircle = new Circle(radius);
+
+                Console.WriteLine();
+                Console.WriteLine("Circumference: " + newCircle.CalculateFormattedCircumference());
+                Console.WriteLine("Area: " + newCircle.CalculateFormattedArea());
+                Console.WriteLine();
+                count++;
+                again = Again();
+            }
+            Console.WriteLine();
+            Console.WriteLine("You created {0} circles.", count);
+        }
+
+        private static bool Again()
+        {
+            bool again;
+            string input;
+
+            Console.WriteLine("Continue? (Y/N)");
+            input = Console.ReadLine();
+            if (input.ToLower() == "y")
+            {
+                again = true;
+            }
+            else if (input.ToLower() == "n")
+            {
+                Console.WriteLine("Goodbye");
+                again = false;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, I couldn't understand your input.  Let's try again.");
+                again = Again();
+            }
+            return again;
+        }
+    }
+}
